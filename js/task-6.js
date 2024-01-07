d6dfcdca82f66e938d73c29e6e10fb38ffff07cf
IhorSlakva <143197433+IhorSlakva@users.noreply.github.com>
2024-01-07 11:44:42 +0200

    homework-07-10

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputEl = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxEl = document.querySelector('#boxes');
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  let value = inputEl.value;
+  if (value < 1 || value > 100) {
+    return;
+  } else {
+    destroyBoxes()
+    let size = 30;
+    for (let i = 0; i < value; i++) {
+      const myElem = document.createElement('div');
+      myElem.style.width = `${size}px`;
+      myElem.style.height = `${size}px`;
+      myElem.style.backgroundColor = getRandomHexColor();
+      boxEl.appendChild(myElem);
+      size += 10;
+    }
+    value = '';
+  }
+}
+
+ function destroyBoxes() {
+    boxEl.innerHTML = '';
+  }
